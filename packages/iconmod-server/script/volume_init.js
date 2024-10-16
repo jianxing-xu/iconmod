b6320d4b2706d15d4c72c0cd9495316825faab9c
Jianxing <xjx_me@yeah.net>
2024-10-16 13:47:33 +0800

    fix: volume init

--- packages/iconmod-server/script/volume_init.js
+++ packages/iconmod-server/script/volume_init.js
@@ -14,26 +14,10 @@ const ICONS_PATH = path.join(dataVolumeDir, 'icons')
 console.log('prismaPath: ', PRISMA_DB)
 console.log('iconsPath: ', ICONS_PATH)
 
-if (await fs.exists(PRISMA_DB) && await fs.pathExists(ICONS_PATH)) {
-  // create baseline migrate
-  // await $`mkdir -p prisma/migrations/0_init`
-  // await $`npx prisma migrate diff \
-  //         --from-empty \
-  //         --to-schema-datamodel prisma/schema.prisma \
-  //         --script > prisma/migrations/0_init/migration.sql`
-  // await $`npx prisma migrate resolve --applied 0_init`
-
-  // deploy migrate
-  await $`npx prisma migrate deploy`
-  console.log('already init. migrate end.')
-}
-else {
+if (!(await fs.exists(PRISMA_DB)) || !(await fs.pathExists(ICONS_PATH))) {
   // create icons dir
   await $`mkdir -p ${ICONS_PATH}`
-
-  console.log('start init prisma.')
-  // create db
-  await $`npx prisma db push`
-
-  console.log('volume init successful!')
+  console.log('create icons dir done.')
 }
+await $`npx prisma migrate deploy`
+console.log('prisma migrate deploy done!')
