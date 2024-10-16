91259bd759f1b588a6440f5bf6dd8f8356a1a94e
Jianxing <xjx_me@yeah.net>
2024-10-16 15:05:41 +0800

    fix

--- packages/iconmod-server/script/volume_init.js
+++ packages/iconmod-server/script/volume_init.js
@@ -14,10 +14,13 @@ const ICONS_PATH = path.join(dataVolumeDir, 'icons')
 console.log('prismaPath: ', PRISMA_DB)
 console.log('iconsPath: ', ICONS_PATH)
 
-if (!(await fs.exists(PRISMA_DB)) || !(await fs.pathExists(ICONS_PATH))) {
+if (!(await fs.pathExists(ICONS_PATH))) {
   // create icons dir
   await $`mkdir -p ${ICONS_PATH}`
   console.log('create icons dir done.')
+}
+if (!(await fs.exists(PRISMA_DB))) {
+  await $`npx prisma migrate dev --name apply` // auto trigger seeding
 }
 await $`npx prisma migrate deploy`
 console.log('prisma migrate deploy done!')
